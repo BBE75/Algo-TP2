354d23cf3f62f48393e99bfe4b3c4f278009999c
BenjaminB <benjamin.beyerle@clirisgroup.com>
2018-12-12 11:13:50 +0100

    Exercice 1 Exercice 2 Question 1 2 3

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import time
 
 
+def sequential_search(tab, valeur, tab_length):
+    i = 0
+    while tab[i] !=  valeur and i < tab_length:
+        i +=1
+    if tab[i] == valeur:
+        print('Valeur: ', valeur,' trouvée après ', i+1, ' comparaisons')
+    else:
+        print('Valeur non présente')
+
+
 def print_tab(tab, tab_length):
     for i in range (0, tab_length):
         print(tab[i], end='')
@@ -28,28 +38,28 @@ def select_sort_tab(tab, tab_length, verbose):
     print("Execution time : %s seconds" % (time.time() - start_time))
 
 
-def dichotomy(tab, min, max, valeur, verbose):
+def dichotomy(tab, min, max, valeur, verbose, cpt):
     length = max - min
+    cpt += 1
     if verbose == 1:
         print('Longueur :', int(length))
         print('min: ', min, 'max: ', max, 'valeur: ', valeur, 'pivot: ', int(min+length/2))
 
     if tab[int(min+length/2)] == valeur:
-        print('Valeur présente à l\'indice: ', int(min+length/2))
+        print('Valeur:', valeur, 'présente à l\'indice: ', int(min+length/2), 'trouvée après', cpt, ' comparaisons')
         return int(min+length/2)
     elif tab[min] == valeur:
-        print('Valeur présente à l\'indice: ', min)
+        print('Valeur:', valeur, 'présente à l\'indice: ', min, 'trouvée après', cpt, ' comparaisons')
         return min
     elif tab[max] == valeur:
-        print('Valeur présente à l\'indice: ', max)
+        print('Valeur:', valeur, 'présente à l\'indice: ', max, 'trouvée après', cpt, 'comparaisons')
         return max
     elif tab[int(min+length/2)] < valeur and length > 1:
         new_min = int(min+length/2)
-        dichotomy(tab, new_min, max, valeur, verbose)
+        dichotomy(tab, new_min, max, valeur, verbose, cpt)
     elif tab[int(min+length/2)] > valeur and length > 1:
         new_max = int(min+length/2)
-        print('max :', new_max)
-        dichotomy(tab, min, new_max, valeur, verbose)
+        dichotomy(tab, min, new_max, valeur, verbose, cpt)
     else:
         print('Valeur non présente dans le tableau')
         return -1
@@ -66,3 +76,5 @@ print_tab(select_tab, len(select_tab))
 select_sort_tab(select_tab, len(select_tab), 0)
 print('Affichage du tableau après tri: ', end='')
 print_tab(select_tab, len(select_tab))
+sequential_search(select_tab, 55, len(select_tab))
+dichotomy(select_tab, 0, len(select_tab)-1, 55, 0, 0)
